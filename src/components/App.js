36ba7c02ba0f640ec245d13bc019536ced7e341f
Yeji-Yoon <angelayoon0102@gmail.com>
2025-02-19 17:51:09 -0800

    s

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+import Links from "./Links";
+
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
@@ -11,8 +13,9 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color={user.color} city={user.city} name={user.name}/>
+      <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin}/>
+      <Links github={user.links.github} linkedin={user.links.linkedin}/>
     </div>
   );
 }
